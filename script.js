6902e141e33530ef7475b37e8cf79b1fff17d0e1
ATARO-QQQ <150531492+ATARO-QQQ@users.noreply.github.com>
2026-07-20 16:39:23 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            
+            bunker: {
+              900: '#0e0f12', 
+              800: '#181a20', 
+              700: '#272930', 
+              600: '#3f424d', 
+              olive: '#4b5340', 
+              rust: '#854d0e', 
+            }
+          },
+          fontFamily: {
+            sans: ['"Zen Kaku Gothic New"', 'sans-serif'],
+            mono: ['"DotGothic16"', 'monospace'],
+          }
+        }
+      }
+    }
